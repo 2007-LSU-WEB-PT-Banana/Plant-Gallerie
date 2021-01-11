3544124ff03795012564973b523e2b0535fff012
Belén menganita <belenbahmerrada@gmail.com>
2021-01-11 14:57:57 -0600

    added basic CSS

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import { getSomething} from '../api';
 import Header from './Header';
+import img from "../images/home-image.jpg";
+import Button from "@material-ui/core/Button";
 
 const App = () => {
   const [message, setMessage] = useState('');
@@ -16,12 +18,69 @@ const App = () => {
       });
   });
 
+const imgStyle = {
+  height: "95vh",
+  width: "100%",
+  backgroundSize: "cover",
+  backgroundPosition: "top",
+  padding : "30px",
+  }
+
+  const headingPrimary = {
+    color: 'black',
+    textTransform: 'uppercase',
+    backfaceVisibility: 'hidden',
+    marginBotton: '60px',
+  }
+
+  const headingMain = {
+    display: 'block',
+    fontSize: '50px',
+    fontWeight: '400',
+    letterSpacing: '20px',
+  }
+
+  const headingMainSub = {
+    display: "block",
+    fontSize: "20px",
+    fontWeight: "400",
+    letterSpacing: "15px",
+  }
+
+  const textBox = {
+    position: 'absolute',
+    top: '20%',
+    left: '10%',
+    textAlign: 'center',
+  }
+
+  const btn = {
+    textDecoration: 'none',
+    textTransform: 'uppercase',
+    padding: '15px 40px',
+
+  }
+
   return (
     <Router>
       <div className="App">
         <Switch>
           <Route path="/">
             <Header />
+            <div className="main-page-image">
+              <img src={img} style={imgStyle} />
+            </div>
+            <div className="text-box" style={textBox}>
+              <h1 className="heading-primary" style={headingPrimary}>
+                <span className="heading-main" style={headingMain}>
+                  buy 2 plants for $200
+                </span>
+                <pan className="heading-main-sub" style={headingMainSub}>
+                  plus free shipping
+                </pan>
+              </h1>
+              <Button className="btn btn-white" style={btn}> Shop Now</Button>
+            </div>
           </Route>
           <Route patch="/houseplants">
             <Header />
@@ -34,6 +93,9 @@ const App = () => {
           </Route>
           <Route path="/login">
             <Header />
+            <Route path="/cart">
+              <Header />
+            </Route>
           </Route>
         </Switch>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,21 @@ import SearchIcon from "@material-ui/icons/Search";
 import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
+const headerStyling = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  background: "rgb(230, 233, 237)",
+  position: "sticky",
+  top: "0",
+  zIndex: "100",
+  border: "2px solid rgb(208, 211, 214)",
+  boxShadow: "2px -1px 2px 0px rgb(108, 184, 108)",
+};
 
 const Header = () => {
   return (
-    <nav className="header">
+    <nav className="header" style={headerStyling}>
       <Link to="/">
         <div className="home-icon">
           <HomeIcon />
@@ -33,10 +44,18 @@ const Header = () => {
         <SearchIcon className="header-searchIcon" />
       </div>
       <div className="log-in">
+        <Link to="/login" className="header-link">
+        <div>
           <PersonOutlineIcon />
+        </div>
+        </Link>
       </div>
       <div className="shopping-cart">
+        <Link to="/cart" className="header-link">
+          <div>
         <ShoppingCartIcon />
+        </div>
+        </Link>
       </div>
     </nav>
   );
