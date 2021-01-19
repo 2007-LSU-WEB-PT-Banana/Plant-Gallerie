cec514faf005c58cb5951b1ae2898dc8ead476f9
fakhan <amank9008@gmail.com>
2021-01-18 19:08:39 -0600

    works

--- src/components/App.js
+++ src/components/App.js
@@ -1,111 +1,113 @@
-import React, { useState, useEffect } from "react";
-import { Switch, Route, useHistory, Link } from "react-router-dom";
-import { fetchAPI, BASE_URL, auth, getToken, clearToken } from "../api";
-import "./Home.css";
+import React, { useState, useEffect } from 'react'
+import { Switch, Route, useHistory, Link } from 'react-router-dom'
+import { fetchAPI, BASE_URL, auth, getToken, clearToken } from '../api'
+import './Home.css'
 import {
-	AllProducts,
-	SingleProduct,
-	Header,
-	Login,
-	FloweringPlants,
-	BonsaiPlants,
-	HousePlants,
-	Home,
-	Register,
-} from "./index";
-import CartComponent from "./Cart";
-import SingleOrder from "./SingleOrder";
+  AllProducts,
+  SingleProduct,
+  Header,
+  Login,
+  FloweringPlants,
+  BonsaiPlants,
+  HousePlants,
+  Home,
+  Register,
+} from './index'
+import CartComponent from './Cart'
+import SingleOrder from './SingleOrder'
 
 const App = () => {
-	const history = useHistory();
+  const history = useHistory()
 
-	const [isLoggedIn, setIsLoggedIn] = useState(!!getToken());
-	const [message, setMessage] = useState("");
-	const [productList, setProductList] = useState([]);
-	const [activeProduct, setActiveProduct] = useState("");
-	const [cartData, setCartData] = useState([]);
-	const [count, setCount] = useState(1);
+  const [isLoggedIn, setIsLoggedIn] = useState(!!getToken())
+  const [message, setMessage] = useState('')
+  const [productList, setProductList] = useState([])
+  const [activeProduct, setActiveProduct] = useState('')
+  const [cartData, setCartData] = useState([])
+  const [count, setCount] = useState(1)
 
-	useEffect(() => {
-		fetchAPI(BASE_URL + "/")
-			.then((response) => {
-				setMessage(response.message);
-			})
-			.catch((error) => {
-				setMessage(error.message);
-			});
-	});
+  useEffect(() => {
+    fetchAPI(BASE_URL + '/')
+      .then((response) => {
+        setMessage(response.message)
+      })
+      .catch((error) => {
+        setMessage(error.message)
+      })
+  })
 
-	useEffect(() => {
-		fetchAPI(BASE_URL + "/products")
-			.then((data) => {
-				data.map((product) => {
-					let newPrice = product.price / 100;
-					product.price = newPrice;
-				});
-				setProductList(data);
-			})
-			.catch(console.error);
-	}, []);
+  useEffect(() => {
+    fetchAPI(BASE_URL + '/products')
+      .then((data) => {
+        data.map((product) => {
+          let newPrice = product.price / 100
+          product.price = newPrice
+        })
+        setProductList(data)
+      })
+      .catch(console.error)
+  }, [])
 
-	console.log("The cart data is", cartData);
+  console.log('The cart data is', cartData)
 
-	return (
-		<>
-			<Header />
-			<main class="wrapper">
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route path={`/products/:productId`}>
-						<SingleProduct
-							activeProduct={activeProduct}
-							setActiveProduct={setActiveProduct}
-							history={history}
-							count={count}
-							setCount={setCount}
-							cartData={cartData}
-							setCartData={setCartData}
-						/>
-					</Route>
-					<Route exact path="/products">
-						<AllProducts
-							productList={productList}
-							history={history}
-							setActiveProduct={setActiveProduct}
-						/>
-					</Route>
-					<Route exact path="/houseplants">
-						<HousePlants
-							productList={productList}
-							setActiveProduct={setActiveProduct}
-							history={history}
-						/>
-					</Route>
-					<Route exact path="/floweringplants">
-						<FloweringPlants
-							productList={productList}
-							setActiveProduct={setActiveProduct}
-							history={history}
-						/>
-					</Route>
-					<Route exact path="/bonsaiplants">
-						<BonsaiPlants
-							productList={productList}
-							setActiveProduct={setActiveProduct}
-							history={history}
-						/>
-					</Route>
-					<Route exact path="/login" component={Login} />
-					<Route exact path="/register">
-						<Register setIsLoggedIn={setIsLoggedIn} />
-					</Route>
-					<Route path="/cart">
-						<CartComponent cartData={cartData} setCartData={setCartData} />
-					</Route>
-				</Switch>
-			</main>
-		</>
-	);
-};
+  return (
+    <>
+      <Header setIsLoggedIn={setIsLoggedIn} />
+      <main class="wrapper">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path={`/products/:productId`}>
+            <SingleProduct
+              activeProduct={activeProduct}
+              setActiveProduct={setActiveProduct}
+              history={history}
+              count={count}
+              setCount={setCount}
+              cartData={cartData}
+              setCartData={setCartData}
+            />
+          </Route>
+          <Route exact path="/products">
+            <AllProducts
+              productList={productList}
+              history={history}
+              setActiveProduct={setActiveProduct}
+            />
+          </Route>
+          <Route exact path="/houseplants">
+            <HousePlants
+              productList={productList}
+              setActiveProduct={setActiveProduct}
+              history={history}
+            />
+          </Route>
+          <Route exact path="/floweringplants">
+            <FloweringPlants
+              productList={productList}
+              setActiveProduct={setActiveProduct}
+              history={history}
+            />
+          </Route>
+          <Route exact path="/bonsaiplants">
+            <BonsaiPlants
+              productList={productList}
+              setActiveProduct={setActiveProduct}
+              history={history}
+            />
+          </Route>
+          <Route exact path="/login">
+            <Login setIsLoggedIn={setIsLoggedIn} />
+          </Route>
+          <Route exact path="/register">
+            <Register setIsLoggedIn={setIsLoggedIn} />
+          </Route>
+          <Route path="/cart">
+            <CartComponent cartData={cartData} setCartData={setCartData} />
+          </Route>
+        </Switch>
+      </main>
+    </>
+  )
+}
 
-export default App;
+export default App
--- src/components/SingleOrder.js
+++ src/components/SingleOrder.js
@@ -1,59 +1,58 @@
-import React from "react";
+import React from 'react'
 import {
-	Button,
-	Card,
-	CardContent,
-	CardActions,
-	Typography,
-	CardActionArea,
-	CardMedia,
-} from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import "./SingleOrder.css";
+  Button,
+  Card,
+  CardContent,
+  CardActions,
+  Typography,
+  CardActionArea,
+  CardMedia,
+} from '@material-ui/core'
+import './SingleOrder.css'
 
 function SingleOrder({ id, price, productName, quantity }) {
-	return (
-		<div
-			style={{
-				display: "flex",
-				flexDirection: "column",
-				flexWrap: "wrap",
-			}}
-			key={id}
-		>
-			<Card
-				variant="outlined"
-				key={price}
-				style={{ height: "270px", width: "650px", margin: "10px" }}
-			>
-				<CardActionArea>
-					<CardMedia
-						component="img"
-						alt="image not found"
-						height="180"
-						width="150"
-						image="https://images.unsplash.com/photo-1453904300235-0f2f60b15b5d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1533&q=80"
-						title="Contemplative Reptile"
-					/>
-					<CardContent>
-						<Typography variant="body2" color="textSecondary" component="p">
-							{productName}
-						</Typography>
-						<Typography variant="body2" color="textSecondary" component="p">
-							{quantity}
-						</Typography>
-						<Typography variant="body2" color="textSecondary" component="p">
-							{price}
-						</Typography>
-					</CardContent>
-				</CardActionArea>
-				<CardActions>
-					<Button size="small"> Add</Button>
-					<Button size="small"> Remove From Cart</Button>
-				</CardActions>
-			</Card>
-		</div>
-	);
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+      }}
+      key={id}
+    >
+      <Card
+        variant="outlined"
+        key={price}
+        style={{ height: '270px', width: '650px', margin: '10px' }}
+      >
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="image not found"
+            height="180"
+            width="150"
+            image="https://images.unsplash.com/photo-1453904300235-0f2f60b15b5d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1533&q=80"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {productName}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {quantity}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {price}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small"> Add</Button>
+          <Button size="small"> Remove From Cart</Button>
+        </CardActions>
+      </Card>
+    </div>
+  )
 }
 
-export default SingleOrder;
+export default SingleOrder
