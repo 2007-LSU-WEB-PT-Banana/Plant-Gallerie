2eeb9c53d58ab7de74686eb6e11faa80cf949b71
deCha <decha92@hotmail.com>
2021-01-29 23:43:23 -0600

    more photo fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,10 +10,13 @@ const Home = () => {
 		"https://cdn-yotpo-images-production.yotpo.com/instagram/26/17845191269080826/standard_resolution.jpg",
 		"https://cdn-yotpo-images-production.yotpo.com/instagram/19/18079210213196619/standard_resolution.jpg",
 		"https://i.pinimg.com/564x/f8/1d/4b/f81d4b822d3d4f2651a66b621db61de7.jpg",
-		"https://lh3.googleusercontent.com/HrY8bVBiw2gfjueqOAdwk5VpQ_a0elPvuo6KNtVvKlLeT7YnzJTmJzpScOCHk5g5WN5_LdxCExB946dKjhMePAxqiMRfXudOuVSld4kUtQIQ9ij8uy_PPQssxfvtG8i5Hyz0NNSeW2yJYlkDsVt10KYTxWaN9ovNdj0wL_Ev040qLgfbmdKqxKH7ggTZJGMiZx9XdO21fWO5Jr-PjDDgCQ4HRXGkXova8Swz--D2NegN3hwdFXKB8raMSYfg0ZJpCCNzGgM9oeE-sFrzVvDLMk929LRYRmgeDjVc0ollh5HPHskRC-xOvzLK3_X4L812tpOfe_V1mSR4_x9iRuAFHho25qvvTD-_zn8l_PmSzVn7TVSipOlGPDX6_m78PDEPVBpXeCrgIpaWVoo52gSZaKap9dccTNUMQ-pSac18j9q7OpA14j5MZF7oPYxY28TaM3EC6A0QKg08IOXELXITSEper51P1PT4zazBrU_kmnlFc4Y5Utrl_93Rm1Btdy_MR-Rx_fh1txt6zNfJCoCifpqBWKhMpM6Y9VyR8LhTmrpLmH8KC0TG8y0cI1iH5m9KKYQ7CcpGL88KhGdBXLVQjzBOHPNGSfmYILHCe94JfytfwZ9-GLUuW6YQwOaKYGbTfJEX0J2SYLo4XH_Zckzw65HX7hu6pyyUwCksYmN8S8Vx9rAdkJsoZiVUTQMjLPI8fJSF0rLC0Tlw00r2S4ZLjJBd=w582-h873-no?authuser=0",
-		"https://lh3.googleusercontent.com/EoarZ5nLeuCTZ6uyH4riJMPQcRxv5U9wkdk7-cBZ-7RUX6W7GP3b4HILrZavxHroIheATKhQEBlT048JMLjMCUVHPY1EefcF3V4e0mSHr0ZKaOodJJoesqSZqNnfV53QFyl2dr3rVEDVbpGdFj1eMWCNxLJXqNBAIOScvKDECtQl-ePbzbgt34WwWEuIUcu0ic4XrcoHnCjFCrpBWMIr4kYckhVGHqq66Vejv3j-TGLlMm08hdvZu0u8g21k-Hcl4OSMnj4gr1IrWEybLfiwnEJgqHoNbsZytTOS3RJOlN6TgjRTkmJShuYq_dqDc2Ky7RHN6t9wOwImoKxsN_hnNMtNDsWrMqHx_b0jnXeAhyPLx_uabGygN9gwGkwT3-0oOtSO38hs_WGBV1DbI8URw9Y1ES54UwKbe7Z4O9hlhsZWq47cqO5YM2fRVEcf03woCHHi4amsdgAOgMSdYbtJyoH3eiYMQJRDn9EnfEkFdusNGUuvMLj6KAYyjJjVWIlx2H9dj_C37d7DyZ3smHv3oITCaZhYgOkELBUiXHtgr3ovNEWZpe7WOesTSOIEDcWCpnsOObzVczyDBJgXWPK4qsdYDIArUsPnxmYU0CLxO0dSVILO8lsZLciD30rqKHgpHOozs2NiajJhLxFr6qlCndgogvjR-2zaPtrs-nbI-049R8PJ29K1jbeGo4nuhdCAOvejdKtKAypK-2QPitRC4_gQ=w1024-h682-no?authuser=0",
+		// "https://modernize.com/wp-content/uploads/2016/02/Decorating-with-Plants-11.jpg",
+		"https://images.squarespace-cdn.com/content/v1/59e32f0f268b9625f4269485/1572201569715-K68FA2CFUAHBP7M6U2EQ/ke17ZwdGBToddI8pDm48kG87Sfbgg29A4BYEDq3OXvgUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcf4OxbJOyh_wHUnyc4kQLQ6SBshRGOku7c30Y_IRDNPta8R2IY5BHMaEj1zOWoDTZ/how-to-create-a-plant-loving-home.jpg?format=1000w",
+		"https://www.plantz.com/wp-content/uploads/2019/07/decorating-with-plants-1024x682.jpeg",
 		"https://i.ytimg.com/vi/31wyZbPARfU/maxresdefault.jpg",
 	];
+	//<a href="https://lh3.googleusercontent.com/c-GJQ6AT3utlu85lgFP6EwkcR6hc1hatrrPDOxtOMK2k7BUfx92yxuS1YifdfL2dpURLTeR2woiImMg7qIDgGr7X_qNAcmXUg4fUxj_0kxJJFAO-A7RKG2IEqqrUQVvFELJDpJhOvQ=w2400?source=screenshot.guru">
+	//<img src="https://lh3.googleusercontent.com/c-GJQ6AT3utlu85lgFP6EwkcR6hc1hatrrPDOxtOMK2k7BUfx92yxuS1YifdfL2dpURLTeR2woiImMg7qIDgGr7X_qNAcmXUg4fUxj_0kxJJFAO-A7RKG2IEqqrUQVvFELJDpJhOvQ=w600-h315-p-k" /> </a>
 	return (
 		<>
 			<section className="homepage">
@@ -52,17 +55,18 @@ const Home = () => {
 								className="slideImage"
 								style={{
 									backgroundImage: `url(${slideImages[4]} )`,
-									height: "100vh",
-									width: "100wh",
+									// height: "100vh",
+									// width: "100wh",
+									// backgroundRepeat: "no-repeat",
+									// backgroundPosition: "center center",
+									backgroundSize: "cover",
 									backgroundRepeat: "no-repeat",
-									backgroundPosition: "center center",
+									backgroundPosition: "center top",
 								}}
 							>
-								<h1 className="header-heading" style={{ color: "white" }}>
-									Plant Gallerie
-								</h1>
+								<h1 className="header-heading">Plant Gallerie</h1>
 								<article className="article-heder">
-									<p style={{ fontSize: "30px", color: "white" }}>
+									<p style={{ fontSize: "30px" }}>
 										Free Shipping With Orders Over $100
 									</p>
 									<button className="main-image-button" id="shopButton">
